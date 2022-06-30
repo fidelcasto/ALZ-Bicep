25e7ca200739e482a8ee236068afb6447d5f347c
Olivier Castonguay <castonguayo@grics.ca>
2022-06-30 11:14:23 -0400

    Ajuster scope déploiement sur RG

--- infra-as-code/bicep/orchestration/hubPeeredSpoke/deploy.grics.ps1
+++ infra-as-code/bicep/orchestration/hubPeeredSpoke/deploy.grics.ps1
@@ -1,7 +1,11 @@
 az login
+$ConnectivitySubscriptionId="450978e3-2c7e-4a77-8303-6846668ac2c3"
+az account set --subscription $ConnectivitySubscriptionId
 
-az deployment mg create `
-    --template-file C:/TFS/GitHub/ALZ-Bicep/infra-as-code/bicep/orchestration/hubPeeredSpoke/hubPeeredSpoke.bicep `
-    --parameters @C:/TFS/GitHub/ALZ-Bicep/infra-as-code/bicep/orchestration/hubPeeredSpoke/parameters/hubPeeredSpoke.parameters.grics.json `
-    --location canadacentral `
-    --management-group-id [...]
+az group create --location canadacentral `
+   --name rgPOCSpoke
+
+az deployment group create `
+    --resource-group rgPOCSpoke `
+    --template-file C:/TFS/GitHub/ALZ-Bicep/infra-as-code/bicep/orchestration/hubPeeredSpoke/hubPeeredSpokeGrics.bicep `
+    --parameters @C:/TFS/GitHub/ALZ-Bicep/infra-as-code/bicep/orchestration/hubPeeredSpoke/parameters/hubPeeredSpoke.parameters.grics.json 
